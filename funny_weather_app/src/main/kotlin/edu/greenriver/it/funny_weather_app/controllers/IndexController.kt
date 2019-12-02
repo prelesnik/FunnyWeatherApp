eea666602fe5d498abeeec5c41fe0ddae404b4bb
Mike <mprelesnik@mail.greenriver.edu>
2019-12-02 14:14:26 -0800

    Modified the temperature to be shown as an int rather than a double

--- funny_weather_app/src/main/kotlin/edu/greenriver/it/funny_weather_app/controllers/IndexController.kt
+++ funny_weather_app/src/main/kotlin/edu/greenriver/it/funny_weather_app/controllers/IndexController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import kotlin.math.roundToInt
 
 /**
  * This controller class provides the route for the index page
@@ -30,8 +31,11 @@ class IndexController
     fun getZipData(model: Model, @RequestParam(name = "zip") zip: String): String
     {
         val service = OpenWeatherService()
-        model.addAttribute("zip", zip)
-        model.addAttribute("weather", service.getWeatherByZip(zip))
+        val weather = service.getWeatherByZip(zip)
+        val tempDouble = (weather.main.temp - 273.15) * 9.0/5 + 32
+        val temp = tempDouble.roundToInt()
+
+        model.addAttribute("temp", temp)
         return "index"
     }
 }
