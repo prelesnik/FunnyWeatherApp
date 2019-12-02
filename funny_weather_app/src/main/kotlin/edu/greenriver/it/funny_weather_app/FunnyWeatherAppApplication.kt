8cbff78dc39488f73c33a9c449d2b27105b84736
Mike <mprelesnik@mail.greenriver.edu>
2019-12-02 10:30:45 -0800

    Initial commit for app to make sure Git upload is successful and accessible by group members

---
+++ funny_weather_app/src/main/kotlin/edu/greenriver/it/funny_weather_app/FunnyWeatherAppApplication.kt
@@ -0,0 +1,11 @@
+package edu.greenriver.it.funny_weather_app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FunnyWeatherAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<FunnyWeatherAppApplication>(*args)
+}
